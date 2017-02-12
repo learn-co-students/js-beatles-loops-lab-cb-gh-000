e7b34b809cb5ddcf14cbb862b7afe5b8d66a4fdc
Wen Bo Xie <wenbox323@gmail.com>
2017-02-12 22:05:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
+  const arr = []
 
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
-  return array
+  return arr
 }
 
 function johnLennonFacts(facts) {
-  var array = []
-  let i = 0
+  const arr = []
 
+  let i = 0
   while(i < facts.length) {
-    array.push(facts[i] + '!!!')
+    arr.push(`${facts[i]}!!!`)
     i++
   }
 
-  return array
+  return arr
 }
 
-function iLoveTheBeatles(number) {
-  var array = []
+function iLoveTheBeatles(n) {
+  const arr = []
 
   do {
-    array.push("I love the Beatles!")
-    number++
-  } while (number < 15);
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15);
 
-  return array
+  return arr
 }
