6659239e21484497a9c65f695764224076b079e7
Nguyen Thuan <ndt0153@gmail.com>
2017-04-10 18:54:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians,instruments) {
+  const temp = [];
+  for(var i=0;i<musicians.length;i++) {
+    temp.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return temp;
+}
+function johnLennonFacts(facts) {
+  var i = 0;
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+/*  while (i < facts.length) {
+    i++;
+    facts[i] = facts + "!!!";
+  }*/
+}
