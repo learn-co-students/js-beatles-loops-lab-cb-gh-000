3af15c70150fb068c189a1f2ede354aa990ecfbb
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 19:35:24 -0400

    Create function johnLennonFacts

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ function theBeatlesPlay(musicians, instruments) {
     let musiciansAndInstruments = [];
 
     for (let i = 0; i < musicians.length; i++) {
-        musiciansAndInstruments.push(`${musicians[i]} plays ${instruments[i]}`)
+        musiciansAndInstruments.push(`${musicians[i]} plays ${instruments[i]}`);
     }
 
     return musiciansAndInstruments;
+}
+
+function johnLennonFacts(facts) {
+    let factsWithExclamations = [];
+
+    let i = 0;
+    while (i < facts.length) {
+        factsWithExclamations.push(`${facts[i]}!!!`);
+        i++;
+    }
+
+    return factsWithExclamations;
 }
