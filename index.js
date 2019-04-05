af579dece85321e87d4fce1c28291b5d486fdbc6
Steffen Albrecht <steffen.alb@posteo.de>
2019-04-05 11:14:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var music=[]
+  for(var i=0;i<musicians.length;i++){
+    music.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return music
+}
+function johnLennonFacts(facts){
+  var counter=0;
+  while(counter<facts.length){
+    facts[counter]+="!!!";
+    counter++;
+  }
+  return facts
+}
+function iLoveTheBeatles(number){
+  var temp=[]
+  do{
+    temp.push("I love the Beatles!");
+    number++;
+  }while(number<15)
+  return temp
+}
