c01448d341faa65c760d832b6abdd21769e0aaa4
Claudiu Andrei Pestenariu <pclandreiuk@gmail.com>
2019-10-24 22:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicArray, instArray){
+  let emptyArr = [];
+  for(let i = 0; i < musicArray.length; i++){
+    emptyArr.push(`${musicArray[i]} plays ${instArray[i]}`);
+  }
+  return emptyArr;
+}
+
+function johnLennonFacts(factArr){
+  let emptyArr2 = [];
+  for(let i = 0; i < factArr.length; i++){
+    emptyArr2.push(`${factArr[i]}!!!`)
+  }
+  return emptyArr2;
+}
+
+function iLoveTheBeatles(num){
+  let emptyArr3 = [];
+  do {
+    emptyArr3.push('I love the Beatles!')
+    num++;
+  } while (15 > num);
+  return emptyArr3;
+}
